90022340a6939c6a693b050ef414d3b020b8fbcc
cherrow <cherrowvideosuggestions@gmail.com>
2024-05-17 23:47:55 -0700

    more css

--- frontend/src/reaping.tsx
+++ frontend/src/reaping.tsx
@@ -8,19 +8,29 @@ function Reaping(): React.ReactElement {
       <table>
         <tbody>
           {[0, 6, 12, 18].map((start, rowIndex) => (
-            <tr key={rowIndex}>
-              {cookie.slice(start, start + 6).map((cookieObj, cellIndex) => (
-                <td key={cellIndex}>
-                  <img src={cookieObj.picture} alt={cookieObj.name} className="tribute-image" />
-                  <p className="tribute-name">{cookieObj.name}</p>
-                </td>
-              ))}
-            </tr>
+            <React.Fragment key={rowIndex}>
+              {/* New row above existing row */}
+              <tr>
+                {[0, 1, 2].map((cellIndex) => (
+                  <td key={cellIndex} colSpan={2} className="district-name">District {rowIndex * 3 + cellIndex + 1}</td>
+                ))}
+              </tr>
+              {/* Existing row */}
+              <tr key={rowIndex + 1000}>
+                {cookie.slice(start, start + 6).map((cookieObj, cellIndex) => (
+                  <td key={cellIndex}>
+                    <img src={cookieObj.picture} alt={cookieObj.name} className="tribute-image" />
+                    <p className="tribute-name">{cookieObj.name}</p>
+                  </td>
+                ))}
+              </tr>
+            </React.Fragment>
           ))}
         </tbody>
       </table>
     </div>
   );
 }
+
 
 export default Reaping;
