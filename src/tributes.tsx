69973bee73efd2829f93d011d787dd713a532d42
cherrow <cherrowvideosuggestions@gmail.com>
2026-04-12 11:46:03 -0700

    update default tributes

--- src/tributes.tsx
+++ src/tributes.tsx
@@ -1,6 +1,5 @@
 //tributes.tsx holds the cookie array, with default values set by cherrow
 
-// Define the types for all cookies
 export type CookieType = {
     name: string;
     isAlive: boolean;
@@ -9,201 +8,33 @@ export type CookieType = {
     picture: string;
     weapon: string;
 };
-  
-// The default cookie array
+
+//default tributes
 const cookie: CookieType[] = [
-    {
-        name: "Caramel Arrow",
-        isAlive: true,
-        health: 120,
-        damage: 60,
-        picture: "https://i.imgur.com/tsQKs7n.png",
-        weapon: "none"
-    },
-    {
-        name: "Fishgato",
-        isAlive: true,
-        health: 100,
-        damage: 50,
-        picture: "https://i.imgur.com/0M5hxK3.png",
-        weapon: "none"
-    },
-    {
-        name: "Milky Way",
-        isAlive: true,
-        health: 70,
-        damage: 50,
-        picture: "https://i.imgur.com/eLJZiWq.png",
-        weapon: "none"
-    },
-    {
-        name: "Strawberry Crepe",
-        isAlive: true,
-        health: 80,
-        damage: 50,
-        picture: "https://i.imgur.com/jdhUz2L.png",
-        weapon: "none"
-    },
-    {
-        name: "Red Velvet",
-        isAlive: true,
-        health: 100,
-        damage: 50,
-        picture: "https://i.imgur.com/yhH2NpW.png",
-        weapon: "none"
-    },
-    {
-        name: "Chiffon",
-        isAlive: true,
-        health: 70,
-        damage: 40,
-        picture: "https://i.imgur.com/5cQS1r2.png",
-        weapon: "none"
-    },
-    {
-        name: "Rye",
-        isAlive: true,
-        health: 100,
-        damage: 60,
-        picture: "https://i.imgur.com/R2phax2.png",
-        weapon: "none"
-    },
-    {
-        name: "Chili Pepper",
-        isAlive: true,
-        health: 90,
-        damage: 60,
-        picture: "https://i.imgur.com/BgXs8MM.png",
-        weapon: "none"
-    },
-    {
-        name: "Licorice",
-        isAlive: true,
-        health: 90,
-        damage: 50,
-        picture: "https://i.imgur.com/HoU9H3O.png",
-        weapon: "none"
-    },
-    {
-        name: "Espresso",
-        isAlive: true,
-        health: 90,
-        damage: 60,
-        picture: "https://i.imgur.com/ovR9w2v.png",
-        weapon: "none"
-    },
-    {
-        name: "Madeleine",
-        isAlive: true,
-        health: 120,
-        damage: 60,
-        picture: "https://i.imgur.com/uW9yIcu.png",
-        weapon: "none"
-    },
-    {
-        name: "Cream Unicorn",
-        isAlive: true,
-        health: 90,
-        damage: 40,
-        picture: "https://i.imgur.com/PA88jOy.png",
-        weapon: "none"
-    },
-    {
-        name: "Tea Knight",
-        isAlive: true,
-        health: 150,
-        damage: 60,
-        picture: "https://i.imgur.com/58UTRKP.png",
-        weapon: "none"
-    },
-    {
-        name: "Robust Axe",
-        isAlive: true,
-        health: 80,
-        damage: 70,
-        picture: "https://i.imgur.com/0EQfqJn.png",
-        weapon: "none"
-    },
-    {
-        name: "Sea Fairy",
-        isAlive: true,
-        health: 90,
-        damage: 60,
-        picture: "https://i.imgur.com/yzGRckx.png",
-        weapon: "none"
-    },
-    {
-        name: "Frost Queen",
-        isAlive: true,
-        health: 80,
-        damage: 70,
-        picture: "https://i.imgur.com/Ofak5Fg.png",
-        weapon: "none"
-    },
-    {
-        name: "Dark Cocoasas",
-        isAlive: true,
-        health: 120,
-        damage: 50,
-        picture: "https://i.imgur.com/jSNvTUn.png",
-        weapon: "none"
-    },
-    {
-        name: "Hollyberry",
-        isAlive: true,
-        health: 150,
-        damage: 50,
-        picture: "https://i.imgur.com/8jsXJyT.png",
-        weapon: "none"
-    },
-    {
-        name: "Pv",
-        isAlive: true,
-        health: 110,
-        damage: 40,
-        picture: "https://i.imgur.com/RVFIsgk.png",
-        weapon: "none"
-    },
-    {
-        name: "White Lily",
-        isAlive: true,
-        health: 100,
-        damage: 60,
-        picture: "https://i.imgur.com/E1UFnoX.png",
-        weapon: "none"
-    },
-    {
-        name: "Golden Cheese",
-        isAlive: true,
-        health: 120,
-        damage: 70,
-        picture: "https://i.imgur.com/zBMyrtc.png",
-        weapon: "none"
-    },
-    {
-        name: "Caprisun",
-        isAlive: true,
-        health: 110,
-        damage: 50,
-        picture: "https://i.imgur.com/lIjChQc.png",
-        weapon: "none"
-    },
-    {
-        name: "Shadow Milk",
-        isAlive: true,
-        health: 100,
-        damage: 60,
-        picture: "https://i.imgur.com/8ztckud.png",
-        weapon: "none"
-    },
-    {
-        name: "Egglair",
-        isAlive: true,
-        health: 90,
-        damage: 40,
-        picture: "https://i.imgur.com/uPyG4hg.png",
-        weapon: "none"
-    }
+    { name: "Pure Vanilla", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/0/09/Cookie_awakened_pure_vanilla_card.png", weapon: "none" },
+    { name: "Gingerbrave", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/b/b5/Cookie_gingerbrave_card.png", weapon: "none" },
+    { name: "Hollyberry", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/c/ce/Cookie_awakened_hollyberry_card.png", weapon: "none" },
+    { name: "Wildberry", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/1/17/Cookie_wildberry_card.png", weapon: "none" },
+    { name: "Dark Cacao", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/a/a4/Cookie_awakened_dark_cacao_card.png", weapon: "none" },
+    { name: "Caramel Arrow", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/e/e9/Cookie_caramel_arrow_card.png", weapon: "none" },
+    { name: "Golden Cheese", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/e/e2/Cookie_awakened_golden_cheese_card.png", weapon: "none" },
+    { name: "Mozzarella", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/3/33/Cookie_mozzarella_card.png", weapon: "none" },
+    { name: "White Lily", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/0/08/Cookie_awakened_white_lily_card.png", weapon: "none" },
+    { name: "Silent Salt", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/7/79/Cookie_silent_salt_card.png", weapon: "none" },
+    { name: "Mystic Flour", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/6/66/Cookie_mystic_flour_card.png", weapon: "none" },
+    { name: "Cloud Haetae", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/f/f2/Cookie_cloud_haetae_card.png", weapon: "none" },
+    { name: "Burning Spice", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/8/84/Cookie_burning_spice_card.png", weapon: "none" },
+    { name: "Nutmeg Tiger", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/b/bb/Cookie_nutmeg_tiger_card.png", weapon: "none" },
+    { name: "Shadow Milk", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/9/9f/Cookie_shadow_milk_card.png", weapon: "none" },
+    { name: "Black Sapphire", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/d/df/Cookie_black_sapphire_card.png", weapon: "none" },
+    { name: "Eternal Sugar", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/9/97/Cookie_eternal_sugar_card.png", weapon: "none" },
+    { name: "Sugarfly", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/f/ff/Cookie_sugarfly_card.png", weapon: "none" },
+    { name: "Sea Fairy", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/9/9a/Cookie_sea_fairy_card.png", weapon: "none" },
+    { name: "Moonlight", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/0/07/Cookie_moonlight_card.png", weapon: "none" },
+    { name: "Wind Archer", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/8/86/Cookie_wind_archer_card.png", weapon: "none" },
+    { name: "Fire Spirit", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/b/ba/Cookie_fire_spirit_card.png", weapon: "none" },
+    { name: "Dark Enchantress", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/a/a7/Cookie_dark_enchantress_card.png", weapon: "none" },
+    { name: "Pomegranate", isAlive: true, health: 100, damage: 50, picture: "https://static.wikia.nocookie.net/cookierunkingdom/images/8/8e/Cookie_pomegranate_card.png", weapon: "none" }
 ];
 
 export default cookie;
